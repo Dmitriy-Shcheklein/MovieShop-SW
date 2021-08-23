36807f5767830a90fe422079787a519b4333fca5
Dmitriy-Shcheklein <dima.sheklein@yandex.ru>
2021-08-23 11:36:10 +0500

    Add css at Header, create CartPage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <header className='header' >
       <Link to='/' className='display-4 main'>SW Fan-Shop</Link>
-      <Link to='/movies' className='display-5'>By a movie</Link>
-      <Link to='/figurine' className='display-5'>Buy a figurine of your favorite charactere</Link>
-      <Link to='/cart' className='fas fa-shopping-cart cart'>Your Cart</Link>
+      <Link to='/movies' className='display-5 movie'>By a movie</Link>
+      <Link to='/figurine' className='display-5 figurine'>Buy a figurine of your favorite charactere</Link>
+      <Link to='/cart' className='fas fa-shopping-cart cart'>&nbsp;Your Cart</Link>
     </header>
   )
 }
--- src/components/Pages/CartPage.js
+++ src/components/Pages/CartPage.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import CartTable from '../CartTable';
+import { useSelector } from 'react-redux';
+
+import './CartPage.scss';
 
 const CartPage = () => {
+
+  const total = useSelector(state => state.allOrder);
+
+  if (!total) {
+    return (
+      <div className='cartPage'>
+        <h2>You have not selected anything!!!</h2>
+        <p>Take your pick.<br /> We have a lot of interesting products.</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      CartPage
-    </div>
+    <CartTable />
   )
 }
 
